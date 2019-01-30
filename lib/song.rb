dc0dc021681af868f6e8c04a0d7e69822db440ad
Stephen Wright <scw248@gmail.com>
2019-01-30 14:55:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,9 @@ require 'pry'
 
 class Song
   extend Memorable::ClassMethods
+  extend Findable::ClassInstances
   include Paramable::InstanceMethods
+  
   attr_accessor :name
   attr_reader :artist
 
@@ -12,9 +14,9 @@ class Song
     @@songs << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@songs
